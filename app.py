8f3da97122079e8ba09aa0af58b6dc448e76bb6e
Shehbaz Patel <60441352+patelshehbaz@users.noreply.github.com>
2024-12-10 00:17:43 -0500

    Update app.py to v5

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Learning DevOps with Git v3"
+    return "Learning DevOps with Git v5"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
